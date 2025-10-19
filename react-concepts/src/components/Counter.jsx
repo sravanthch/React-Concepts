0c1f216a63ab936cc62368c051d70e7124a43322
Sravanth Ch <sravanthch18@gmail.com>
2025-10-19 11:27:37 +0530

    Home Redirection

--- react-concepts/src/components/Counter.jsx
+++ react-concepts/src/components/Counter.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import { Button } from 'primereact/button';
-        
+import RedirectHome from './RedirectHome';
+
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   return (
     <div className='home-header'>
-        <h2>{count}</h2>
-        <Button label="-" onClick={()=>setCount(count-1)}/>
-        <Button label="+" onClick={()=>setCount(count+1)}/>
+      <h2>{count}</h2>
+      <Button label="-" onClick={() => setCount(count - 1)} />
+      <Button label="+" onClick={() => setCount(count + 1)} />
+      <div>
+        <RedirectHome />
+      </div>
     </div>
   )
 }
